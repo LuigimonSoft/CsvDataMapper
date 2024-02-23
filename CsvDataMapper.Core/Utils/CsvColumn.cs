55513694ee97e1ba958d135ff7e73e479e859b00
Luis Carlos <almadejaguar@hotmail.com>
2024-02-23 01:30:25 -0500

    #11 Add map to model methode

---
+++ CsvDataMapper.Core/Utils/CsvColumn.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvDataMapper.Core.Utils
+{
+    internal class CsvColumn
+    {
+        public string Name { get; set; }
+        public int Index { get; set; }
+        public int size { get; set; }
+        public PropertyInfo PropertyInfo { get; set; }
+
+        public CsvColumn()
+        {
+            Index = -1;
+        }
+
+    }
+}
