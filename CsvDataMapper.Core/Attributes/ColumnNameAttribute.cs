98919d3ad9a0705b5e67b81b91ccc774a6108806
Luis Carlos <almadejaguar@hotmail.com>
2024-02-23 02:44:43 -0500

    Fix several errors

--- CsvDataMapper.Core/Attributes/ColumnNameAttribute.cs
+++ CsvDataMapper.Core/Attributes/ColumnNameAttribute.cs
@@ -7,10 +7,10 @@ using System.Threading.Tasks;
 namespace CsvDataMapper.Core.Attributes
 {
     [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
-    public class ColumNameAttribute: Attribute
+    public class ColumnNameAttribute: Attribute
     {
         public string ColumnName { get; }
-        public ColumNameAttribute(string columnName)
+        public ColumnNameAttribute(string columnName)
         {
             ColumnName = columnName;
         }
